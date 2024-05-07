1da6a719283cc0cb3aff81c1721c35075b612b1b
snoo <snoo@yaylabs.io>
2024-05-07 22:05:12 +0900

    no message

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import WebApp from '@twa-dev/sdk';
-
+//test bot
 WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
